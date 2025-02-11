3adc6b5be7bd49d5109ae655852edb3d10f379cf
Daniele Adelfio <daniele.adelfio@gmail.com>
2025-02-11 17:54:24 +0100

    add final list

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
 //App.tsx rappresenta il root
 import Message from "./Message";
 import ListGroup from "./components/ListGroup";
+import ListGroupWithInterface from "./components/ListGroupWithInterface";
 
 function App() {
   //return <div><Message></Message></div>
   //Uguale a sopra ma più conciso
   //return <div><Message /></div>;
 
+  const items = ["NewYork", "Milan", "Paris", "Tokyo", "London", "Palermo"];
   return (
     <div>
-      <ListGroup />
+      <ListGroupWithInterface items={items} heading="Cities" />
     </div>
   );
+
+  /*
+  i vari useState in react come quello presente in ListGroup.tsx sono indipendenti e mantengono il loro stato 
+  anche cross classi questo significa che se aggiungessi dentro il tag div un altro <ListGroup> 
+  accanto a quello già presente duplicherei il rendering di quella classe ma lo state rimarrebbe indipendente e permanente
+  in concreto: se ho uno useState su un Onclick che mi segna quando un elemento della lista è clickato ed attivo
+  duplicando la classe dentro App.tsx potrei avere nella stessa pagina due elementi cliccati e attivi contemporaneamente,
+  indipendenti l'uno dall'altro
+
+  */
 }
 
 export default App;
---
+++ src/components/ListGroupWithInterface.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+interface ListGroupProps {
+  items: string[];
+  heading: string;
+}
+
+/*creo una funzione che accetta props che è di tipo ListGroupProps che contiene 2 attributi.
+il problema di richiamare l'interfaccia in questo modo è che devo sempre accedere a props e poi col . 
+recuperare l'attributo che mi interessa sicuramente utile ma nel mio caso avendo solo due elementi posso 
+già direttamente dove dichiaro i parametri di input della funzione ListGroupWithProps "spaccare" l'oggetto dichiarando due parametri
+Es: da -> function ListGroupWithProps(props: ListGroupProps) {}
+Es: a -> function ListGroupWithProps({items, heading}: ListGroupProps) {}
+*/
+function ListGroupWithInterface({ items, heading }: ListGroupProps) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroupWithInterface;
